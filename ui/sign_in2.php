4d519b2475e6d2146c345485e6567e015adbc47e
B.Leel Vinod <leelvinod20@gmail.com>
2016-10-27 15:26:23 +0530

    same old

---
+++ ui/sign_in2.php
@@ -0,0 +1,52 @@
+<html><head>
+<title> welcome page</title>
+<style>
+.button {
+    background-color: #4CAF50; 
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+.button1 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #4CAF50;
+}
+
+.button2 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #008CBA;
+}
+#buttton {
+margin-top:150px;    
+margin-left:500px;
+    }
+.home {
+   text-align:center;
+}
+
+
+</style>
+</head><body style="background-color:White"><div class="home">
+<h1> WELCOME TO MY HOME-PAGE </h1>
+</div>
+
+
+<div id="buttton">
+<a href="http://127.0.1.1/sign_in1.php"> <button class="button button1">sign_in</button>
+</a><a href="http://127.0.1.1/sign_in.php"> <button class="button button2">log_in</button>
+</a></div><a href="http://127.0.1.1/sign_in.php">
+
+
+
+
+
+</a></body></html>
